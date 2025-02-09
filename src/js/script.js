2fe397dd48c59d241243561a2be1379464a1c35a
Muhammad Nurul Ikhwan <72797830+mnikhwan@users.noreply.github.com>
2025-02-09 09:39:11 +0700

    Update script.js ♻

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 // Toggle mobile menu visibility
 document.getElementById('menu-toggle').addEventListener('click', function () {
     const mobileMenu = document.getElementById('mobile-menu');
+    const logobtn = document.getElementById('mobile-menu');
     mobileMenu.classList.toggle('hidden');
   });
